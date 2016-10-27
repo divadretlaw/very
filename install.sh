4726e427ef8099813cde23e17bb9a39755864598
David Walter <walter.david@me.com>
2016-10-27 10:05:18 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,15 @@
 mkdir -p $HOME/.config/very/
 cp very.json $HOME/.config/very/very.json
 
+if [ $(which fish) != "" ]; then
 # fish
 mkdir -p $HOME/.config/fish/completions/
 mkdir -p $HOME/.config/fish/functions/
 cp fish/completions/very.fish $HOME/.config/fish/completions/very.fish
 cp fish/functions/very.fish $HOME/.config/fish/functions/very.fish
+else
+#bash
+echo 'alias very="python $HOME/.very.py"' >> $HOME/.bashrc
+fi
 
-curl -Lko $HOME/.very.py http://davidwalter.at/d/very.py
+curl -SLko $HOME/.very.py https://raw.githubusercontent.com/divadretlaw/very/master/very.py
