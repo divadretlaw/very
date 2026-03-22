fe395ecd6565d658ada7adceb167adcca438d62e
David Walter <hello@davidwalter.at>
2026-03-22 09:47:55 +0100

    Update

--- Sources/very/Very.swift
+++ Sources/very/Very.swift
@@ -28,7 +28,7 @@ import Shell
         return CommandConfiguration(
             commandName: "very",
             abstract: "very",
-            version: "3.4.0",
+            version: "3.5.0",
             subcommands: subcommands
         )
     }()
